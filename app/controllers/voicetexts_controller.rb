9194a3b2df20fc76e04d790745e13f2e7af0d4b8
sandonemaki <kamuramaki@gmail.com>
2022-08-12 00:45:17 +0900

    取得したJSONデータから、日本語テキスト（改行ありなし）と英語テキスト（改行ありなし）に整形するメソッドを追加した

--- app/controllers/voicetexts_controller.rb
+++ app/controllers/voicetexts_controller.rb
@@ -1,5 +1,67 @@
 class VoicetextsController < ApplicationController
   def new
-    hash = OcrApi.call_ocr_api
+    @hash = OcrApi.call_ocr_api
+
+    # 欧文_改行なし
+    def read_english
+      en_lines = @hash["analyzeResult"]["readResults"][0]["lines"]#[0]["text"]
+      en_line_ary = []
+      en_lines.each do |line|
+        en_line_ary.push(line["text"])
+      end
+      # print
+      en_line_ary.join(" ")
+    end
+
+    # 欧文_改行あり
+    def read_english_line_break
+      en_lines = @hash["analyzeResult"]["readResults"][0]["lines"]
+      # en_line_ary = []
+      en_lines.each do |line|
+        # puts
+        line["text"]
+      end
+    end
+
+    # 日本語横組み_改行なし
+    def read_ja
+      ja_lines = @hash["analyzeResult"]["readResults"][0]["lines"]
+      ja_line_ary = []
+      ja_lines.each do |line|
+        ja_line_ary.push(line["text"])
+      end
+      # print
+      ja_line_ary
+    end
+
+    # 日本語横組み_改行あり
+    def read_ja_line_break
+      ja_lines = @hash["analyzeResult"]["readResults"][0]["lines"]
+      # ja_line_ary = []
+      ja_lines.each do |line|
+        # puts
+        line["text"]
+      end
+    end
+
+    # 日本語縦組み_改行あり
+    def read_ja_vertical_line_break
+      ja_lines = @hash["analyzeResult"]["readResults"][0]["lines"]
+      ja_line_ary = []
+      ja_lines.each do |line|
+        ja_line_ary.push(line["text"])
+      end
+      puts ja_line_ary.reverse!
+    end
+
+    # 日本語縦組み_改行なし
+    def read_ja_vertical
+      ja_lines = @hash["analyzeResult"]["readResults"][0]["lines"]
+      ja_line_ary = []
+      ja_lines.each do |line|
+        ja_line_ary.push(line["text"])
+      end
+      print ja_line_ary.reverse!.join("")
+    end
   end
 end
