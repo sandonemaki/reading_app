df32a83aea97f2e0fd8643f5c1ad783856d279b0
sandonemaki <kamuramaki@gmail.com>
2022-08-15 19:45:40 +0900

    voicetext.html.erbでリダイレクトした場合、トップページに遷移するようにした。

--- app/controllers/voicetexts_controller.rb
+++ app/controllers/voicetexts_controller.rb
@@ -1,8 +1,12 @@
 class VoicetextsController < ApplicationController
-  def create
-    image_url = params[:image_url]
-    voicetext = OcrApi.call_ocr_api(image_url)
-    new_view_model = VoicetextViewModel::NewViewModel.new(voicetext: voicetext)
-    render("new", locals:{voicetext: new_view_model})
+  def new
+    if params[:image_url]
+      image_url = params[:image_url]
+      voicetext = OcrApi.call_ocr_api(image_url)
+      new_view_model = VoicetextViewModel::NewViewModel.new(voicetext: voicetext)
+      render("new", locals:{voicetext: new_view_model})
+    else
+      redirect_to("/")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'pageimages/show' => 'pageimages#show'
+  post 'voicetexts/new' => 'voicetexts#new'
   get 'voicetexts/new' => 'voicetexts#new'
-  post 'voicetexts/create' => 'voicetexts#create'
   root to: 'pageimages#show'
 end
