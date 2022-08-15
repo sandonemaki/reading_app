7bd30e3962be7354327a1d928f75b1223d9a6a49
sandonemaki <kamuramaki@gmail.com>
2022-08-15 14:36:08 +0900

    voicetextモデルを作成したがdbを取り扱わないので削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_08_13_074304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "voicetexts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
